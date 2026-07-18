aa814921de14c88389e1fedcadcb7ee4336630b2
megaj05 <mega0520j@gmail.com>
2026-07-18 10:50:54 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,8 +7,10 @@
  */
 
 import axios from "axios";
+const BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://Mega05102020.pythonanywhere.com/api";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const client = axios.create({
   baseURL: BASE_URL,
